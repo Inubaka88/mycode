874cbe8980009ff1d3b4fb4410aa4f170520a57a
Inubaka88 <christian.a.gil96@gmail.com>
2024-01-03 19:16:02 +0000

    manipulating lists

---
+++ lab19.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+wordbank= ["indentatioin", "spaces"]
+
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
+wordbank.append(4)
+
+num = int(input("Please input a number between 0 and 20"))
+
+student_name= tlgstudents[num]
+
+<student_name> always uses <4> <spaces> to indent.
+
---
+++ listmethods/listmeth01.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+proto = ["ssh", "http", "https"]
+
+protoa = ["ssh", "http", "https"]
+
+print(proto)
+proto.append("dns")
+protoa.append("dns")
+
+print(proto)
+
+protoa.reverse()
+
+proto2= [22, 80, 443, 53]
+
+proto.extend(proto2)
+
+print(proto)
+
+protoa.append(proto2)
+
+print(protoa)
+
+print(proto.count("http"))
+
+
