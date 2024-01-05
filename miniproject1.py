c77d5c620d7b28b4118dfb420c27fb12c82d9cd2
Inubaka88 <christian.a.gil96@gmail.com>
2024-01-05 17:12:45 +0000

    A bit on error handling

---
+++ miniproject1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+questions = [{"nintendo": "mario", "legend of zelda", "pokemon"} {"xbox": "gta", "cod", "halo"} {"playstation": "gow", "resident evil"]
+
+def main():
+    correct_answers = 0
+    incorrect_answers = 0
+    while correct_answers <= 5 or incorrect_answers > 5:
+        print(input("Choose your system: nintendo, xbox, playstion\n>")).lower()
+        64_sys = 'nintendo'
+        x_sys = 'xbox'
+        play_sys = 'playstation'
+        if 
+            if wrong_answer == incorrect_answer:
+                print("Sorry you got that answer wrong")
+                incorrect_answers = incorrect_answers + 1
+            else right_answer == correct_answers:
+                print("Correct!")
+                correct_answers == correct_answers +1
+
+main()
